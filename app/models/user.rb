25b9127932ca3ae2f2809be19af945627a15ca69
aadarsh-ongraph <aadarshongraph64@gmail.com>
2023-07-17 15:11:33 +0530

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :feeds
+  has_many :likes
+  has_many :comments
+  has_many :group_members
+  has_many :groups, through: :group_members
+  has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
+  has_many :sent_shared_feeds, class_name: 'SharedFeed', foreign_key: 'sender_id'
+  has_many :received_shared_feeds, class_name: 'SharedFeed', foreign_key: 'receiver_id'
 end
